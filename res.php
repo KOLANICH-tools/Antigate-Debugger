451dff82b91647061894ef5e2febffe78fc68fcc
KOLANICH <kolan_n@mail.ru>
2011-06-07 12:21:50 -0700

    fixed a little

--- res.php
+++ res.php
@@ -1,27 +1,11 @@
 <?
-error_reporting(E_ALL);
-include_once("../serverdata.inc");
-include_once($modulesdir."dBug/dBug.php");
-
 $action=$_REQUEST["action"];
 $key=$_REQUEST["key"];
 $key=hexdec($key);
 $id=$_REQUEST["id"];
 $ids=$_REQUEST["ids"];
-
-session_start();
-if($action=="clear"){
-	session_destroy();
-	die("cleared");
-}
-if(empty($key)||empty($action)||(empty($id)&&empty($ids)) ){
-	die("PARAMETER_MISSED");
-}
 if($key%2)die("ERROR_KEY_DOES_NOT_EXIST");
-
-
-//new dBug($_SESSION);
-
+session_start();
 function getCaptcha($id,$single=0){
 	//print_r($id);
 	$id=(int)$id;
@@ -32,11 +16,21 @@ function getCaptcha($id,$single=0){
 		else return "CAPCHA_NOT_READY";
 	}else return "ERROR_NO_SUCH_CAPCHA_ID";
 }
+function abuse($id){
+	if(isset($_SESSION["captchas"][$id])){
+		return "OK_REPORT_RECORDED";
+	}else return "ERROR_NO_SUCH_CAPCHA_ID";
+}
 
+if($action=="clear"){
+	session_destroy();
+	die("cleared");
+}
+if(empty($key)||empty($action))die("PARAMETER_MISSED");
 $output=array();
-
 switch($action){
 	case "get":
+		if(empty($id)&&empty($ids))die("PARAMETER_MISSED");
 		if(isset($ids)){
 			$ids=explode(',', $ids);
 			//new dBug($ids);
@@ -49,8 +43,12 @@ switch($action){
 			
 		}
 	break;
-	case "getBalance":
-		die(rand()*1000);
+	case "getbalance":
+		echo rand()/getrandmax()*100;
+	break;
+	case "reportbad":
+		if(empty($id))die("PARAMETER_MISSED");
+		echo(abuse($id));
 	break;
 }
 
